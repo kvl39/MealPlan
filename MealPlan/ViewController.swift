abbd5a78ff8d4f4001454e227996675df6ce7234
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-04-30 15:27:00 +0800

    Project setup

---
+++ MealPlan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MealPlan
+//
+//  Created by Liao Kevin on 2018/4/30.
+//  Copyright © 2018年 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
