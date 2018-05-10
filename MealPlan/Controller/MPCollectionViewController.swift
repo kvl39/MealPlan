6651034dedaef9eb3c69d675d54166c48ee05161
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-10 15:32:23 +0800

    Add function: modify horizontal collection view to card view

--- MealPlan/Controller/MPCollectionViewController.swift
+++ MealPlan/Controller/MPCollectionViewController.swift
@@ -30,7 +30,22 @@ extension MPCollectionViewController: UICollectionViewDelegate, UICollectionView
         let subView = viewArray[indexPath.row]
         subView.tag = 2
         cell.collectionViewCell.addSubview(subView)
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            subView.leadingAnchor.constraint(equalTo: cell.collectionViewCell.leadingAnchor, constant: 10),
+            subView.trailingAnchor.constraint(equalTo: cell.collectionViewCell.trailingAnchor, constant: -10),
+            subView.topAnchor.constraint(equalTo: cell.collectionViewCell.topAnchor, constant: 10),
+            subView.bottomAnchor.constraint(equalTo: cell.collectionViewCell.bottomAnchor, constant: -40),
+            ])
         return cell
     }
     
+}
+
+
+extension MPCollectionViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: 100)
+    }
 }
--- MealPlan/View/HorizontalCollectionViewCell/HorizontalCollectionViewCell.swift
+++ MealPlan/View/HorizontalCollectionViewCell/HorizontalCollectionViewCell.swift
@@ -11,11 +11,22 @@ import UIKit
 class HorizontalCollectionViewCell: UICollectionViewCell {
 
    
-   
-    
+
+    @IBOutlet weak var cardLabel: UILabel!
     @IBOutlet weak var collectionViewCell: UIView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        collectionViewCell.backgroundColor = UIColor.white
+        //contentView.backgroundColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
+        collectionViewCell.layer.cornerRadius = 3.0
+        collectionViewCell.layer.masksToBounds = false
+        collectionViewCell.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+        collectionViewCell.layer.shadowOffset = CGSize(width: 0, height: 0)
+        collectionViewCell.layer.shadowOpacity = 0.8
+  
     }
 
     
