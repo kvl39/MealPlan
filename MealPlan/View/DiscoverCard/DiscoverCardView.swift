fe76b721f392002235e6a31122862cec23fce604
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-17 21:50:41 +0800

    Add Card View for discovery page

---
+++ MealPlan/View/DiscoverCard/DiscoverCardView.swift
@@ -0,0 +1,34 @@
+//
+//  DiscoverCardView.swift
+//  MealPlan
+//
+//  Created by Liao Kevin on 2018/5/17.
+//  Copyright © 2018年 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverCardView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var cardMainImage: UIImageView!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("DiscoverCardView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+}
