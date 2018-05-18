9f8d8a878af548e7503f48c50c9d03f31b351fa8
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-18 14:21:28 +0800

    Refactor code for card stack

--- MealPlan/Controller/DiscoveryPageViewController.swift
+++ MealPlan/Controller/DiscoveryPageViewController.swift
@@ -10,25 +10,29 @@ import UIKit
 
 class DiscoveryPageViewController: UIViewController {
 
-    @IBOutlet weak var cardView: DiscoverCardView!
+    
     
     var cardArray: [DiscoverCardView] = []
     var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        cardView.cardMainImage.image = #imageLiteral(resourceName: "world")
-        cardView.alpha = 0
+        configureInitialCardStack()
+        addGestureRecognizer()
+    }
+    
+    func configureInitialCardStack() {
         for i in 0...2 {
             let newCardView = DiscoverCardView(frame: CGRect(x: view.center.x - 150 + CGFloat(i*20), y: view.center.y - 200 + CGFloat(i*20), width: 300, height: 400))
             newCardView.cardMainImage.image = #imageLiteral(resourceName: "pig")
             newCardView.hint.text = "\(counter)"
             counter += 1
             cardArray.append(newCardView)
-            //self.view.addSubview(newCardView)
-            self.view.insertSubview(newCardView, at: 2-i)
+            self.view.insertSubview(newCardView, at: 0)
         }
+    }
+    
+    func addGestureRecognizer() {
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognized(_:)))
         cardArray[0].addGestureRecognizer(panGestureRecognizer)
     }
@@ -59,18 +63,14 @@ class DiscoveryPageViewController: UIViewController {
                 UIView.animate(withDuration: 0.3) {
                     card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 50)
                     card.alpha = 0
-                    self.cardArray[0].gestureRecognizers?.removeAll()
-                    self.cardArray.remove(at: 0)
-                    self.createNewCard()
+                    self.configureCardTransition()
                 }
                 return
             } else if card.center.x > (view.frame.width - 75) {
                 UIView.animate(withDuration: 0.3) {
                     card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 50)
                      card.alpha = 0
-                    self.cardArray[0].gestureRecognizers?.removeAll()
-                    self.cardArray.remove(at: 0)
-                    self.createNewCard()
+                    self.configureCardTransition()
                 }
                 return
             }
@@ -81,9 +81,10 @@ class DiscoveryPageViewController: UIViewController {
         }
     }
     
-    func createNewCard() {
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognized(_:)))
-        self.cardArray[0].addGestureRecognizer(panGestureRecognizer)
+    func configureCardTransition() {
+        self.cardArray[0].gestureRecognizers?.removeAll()
+        self.cardArray.remove(at: 0)
+        addGestureRecognizer()
         
         let newCardView = DiscoverCardView(frame: CGRect(x: view.center.x - 150 + 40, y: view.center.y - 200 + 40, width: 300, height: 400))
         newCardView.cardMainImage.image = #imageLiteral(resourceName: "pork")
@@ -98,7 +99,6 @@ class DiscoveryPageViewController: UIViewController {
             self.cardArray[1].transform = CGAffineTransform(translationX: -20, y: -20)
             newCardView.alpha = 1
         }
-
     }
     
 
