d49a8bbd3161edbbf14f22449f768d68a3a42cf3
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-23 17:31:44 +0800

    Fix bug: configue cell information in willDisplay since willDisplay method executes everytime However, cellForRow method doesn't execute everytime

--- MealPlan/Controller/MPCollectionViewController.swift
+++ MealPlan/Controller/MPCollectionViewController.swift
@@ -24,21 +24,9 @@ extension MPCollectionViewController: UICollectionViewDelegate, UICollectionView
 
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         
-        print("%%%%%Cell will display row \(indexPath.row)%%%%%")
+        guard let cell = cell as? HorizontalCollectionViewCell else { return }
         
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HorizontalCollectionViewCell", for: indexPath) as? HorizontalCollectionViewCell else {
-            return UICollectionViewCell()
-            
-        }
-        
-//        cell.collectionViewCell.subviews.map{ $0.removeFromSuperview() }
         let subImageview = viewArray[indexPath.row]
-        print("---------index Path cell for row \(indexPath.row)--------")
-        print(subImageview.superview ?? "No super view")
         subImageview.layer.cornerRadius = 5
         subImageview.layer.masksToBounds = true
         subImageview.translatesAutoresizingMaskIntoConstraints = false
@@ -49,28 +37,18 @@ extension MPCollectionViewController: UICollectionViewDelegate, UICollectionView
             subImageview.topAnchor.constraint(equalTo: cell.collectionViewCell.topAnchor, constant: 10),
             subImageview.bottomAnchor.constraint(equalTo: cell.collectionViewCell.bottomAnchor, constant: -10)
             ])
-//        cell.collectionViewCell.layoutIfNeeded()
-//        if let viewWithTag = cell.viewWithTag(2) {
-//            viewWithTag.removeFromSuperview()
-//        }
-//        let subView = viewArray[indexPath.row]
-//        subView.layer.cornerRadius = 5
-//        subView.layer.masksToBounds = true
-//        subView.tag = 2
-//        cell.collectionViewCell.addSubview(subView)
-//        subView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            subView.leadingAnchor.constraint(equalTo: cell.collectionViewCell.leadingAnchor, constant: 10),
-//            subView.trailingAnchor.constraint(equalTo: cell.collectionViewCell.trailingAnchor, constant: -10),
-//            subView.topAnchor.constraint(equalTo: cell.collectionViewCell.topAnchor, constant: 10),
-//            subView.bottomAnchor.constraint(equalTo: cell.cardLabel.topAnchor, constant: -10)
-//            ])
-//        cell.cardLabel.text = titleArray[indexPath.row]
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HorizontalCollectionViewCell", for: indexPath) as? HorizontalCollectionViewCell else {
+            return UICollectionViewCell()
+        }
         return cell
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        print("@@@@@@@@end display cell for row \(indexPath.row)@@@@@@@@")
         viewArray[indexPath.row].removeFromSuperview()
     }
     
