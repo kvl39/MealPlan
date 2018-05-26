746c44b7a44761ba16aae0c5f2d98286ac08a09f
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-26 18:01:04 +0800

    Add function: a scroll view with title, and the title has parallax effect by calculating in the scollViewDidScroll function

---
+++ MealPlan/Controller/MPHorizontalScrollViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MPHorizontalScrollViewController.swift
+//  MealPlan
+//
+//  Created by Liao Kevin on 2018/5/26.
+//  Copyright © 2018年 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class MPHorizontalScrollViewController: UIViewController, UIScrollViewDelegate {
+
+    let horizontalScrollView = MPHorizontalScrollView()
+    let viewArray = [UIView]()
+    let numberOfScrolls = 3
+    let scrollViewLabel = ["肉類", "蔬菜", "水果"]
+    let viewTagOffset = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 20, height: 200)
+        configureScrollView()
+    }
+
+    func configureScrollView() {
+        self.horizontalScrollView.horizontalScrollView.delegate = self
+        self.view.addSubview(horizontalScrollView)
+        horizontalScrollView.frame = self.view.frame
+        horizontalScrollView.horizontalScrollView.contentSize.width = self.view.frame.width * CGFloat(numberOfScrolls)
+        for index in 0...numberOfScrolls-1 {
+            let view = UIView(frame: CGRect(x: 10 + self.view.frame.width * CGFloat(index), y: 40, width: self.view.frame.width - 20, height: horizontalScrollView.horizontalScrollView.frame.height-40))
+            view.backgroundColor = UIColor.red
+            view.tag = index + viewTagOffset
+            self.horizontalScrollView.horizontalScrollView.addSubview(view)
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 5, width: 0, height: 40))
+            label.text = scrollViewLabel[index]
+            label.font = UIFont.boldSystemFont(ofSize: 30)
+            label.sizeToFit()
+            label.tag = 10 + index + viewTagOffset
+            if index == 0 {
+                label.center.x = view.center.x
+            } else {
+                label.center.x = view.center.x - self.horizontalScrollView.horizontalScrollView.frame.width / 2
+            }
+            
+            self.horizontalScrollView.horizontalScrollView.addSubview(label)
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == self.horizontalScrollView.horizontalScrollView{
+            for i in 0..<numberOfScrolls {
+                let label = horizontalScrollView.horizontalScrollView.viewWithTag(i + 10 + viewTagOffset) as! UILabel
+                let view = horizontalScrollView.horizontalScrollView.viewWithTag(i + viewTagOffset) as! UIView
+                let scrollContentOffset = horizontalScrollView.horizontalScrollView.contentOffset.x + horizontalScrollView.horizontalScrollView.frame.width
+                
+                var viewOffsets = (view.center.x - horizontalScrollView.horizontalScrollView.bounds.width / 4) - scrollContentOffset
+                label.center.x = scrollContentOffset - ((horizontalScrollView.horizontalScrollView.bounds.width/4 - viewOffsets)/2)
+                
+            }
+        }
+    }
+    
+}
---
+++ MealPlan/View/MPHorizontalScrollView/MPHorizontalScrollView.swift
@@ -0,0 +1,34 @@
+//
+//  MPHorizontalScrollView.swift
+//  MealPlan
+//
+//  Created by Liao Kevin on 2018/5/26.
+//  Copyright © 2018年 Kevin Liao. All rights reserved.
+//
+
+import UIKit
+
+class MPHorizontalScrollView: UIView {
+
+    
+    @IBOutlet weak var horizontalScrollView: UIScrollView!
+    @IBOutlet var contentView: UIView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("MPHorizontalScrollView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
