364b0b8c0f0edddf8e44819a15a2ff14f0337d9b
Liao Kevin <liaokevin@Liaode-MacBook-Pro.local>
2018-05-28 12:23:30 +0800

    Modify UI: plan quote is shorter Modify UI: the position of start plan button

--- MealPlan/Support/HintLabels.swift
+++ MealPlan/Support/HintLabels.swift
@@ -11,9 +11,9 @@ import Foundation
 
 struct HintLabels {
     var hintLabels = [
-        "All you need is the plan, the road map, and the courage to press on to your destination. - Earl Nightingale",
-        "You were born to win, but to be a winner, you must plan to win, prepare to win, and expect to win. - Zig Ziglar",
-        "Plan your work for today and every day, then work your plan. - Margaret Thatcher",
+        "It takes as much energy to wish as it does to plan. - Eleanor Roosevelt",
+        "Failing to plan is planning to fail. - Alan Lakein",
+        "It takes as much energy to wish as it does to plan. - Eleanor Roosevelt",
         "Everyone has a plan 'till they get punched in the mouth. - Mike Tyson"
     ]
 }
